728eb876ee72483dfb42a1a18cc8dd088247d552
simon <6114650+SuperCorks@users.noreply.github.com>
2024-04-09 11:12:15 -0400

    Auto commit on Tue  9 Apr 2024 11:12:15 EDT

--- autocommit.sh
+++ autocommit.sh
@@ -14,16 +14,17 @@ fi
 # Function to handle changes
 commit_changes() {
 
-    # Get the git diff for the specified file
-    diff_output=$(git diff -- karabiner.json)
+    git add .
+    # Get the git diff for all staged files
+    diff_output=$(git diff --cached)
 
     # Remove lines that match '+    "selected"' or '-    "selected"'
     # using egrep with the --invert-match option to exclude these lines
     filtered_diff=$(echo "$diff_output" | egrep --invert-match '^\+(    )*\"selected\"|^\-(    )*\"selected\"')
 
     # Count the remaining + and - lines to determine if there are any significant changes
-    plus_lines=$(echo "$filtered_diff" | grep -c '^\+')
-    minus_lines=$(echo "$filtered_diff" | grep -c '^\-')
+    plus_lines=$(echo "$filtered_diff" | grep -c '^\+[^+]')
+    minus_lines=$(echo "$filtered_diff" | grep -c '^\-[^-]')
 
     # Only commit if there are more remaining + or - lines
     if [[ $plus_lines -eq 0 ]] && [[ $minus_lines -eq 0 ]]; then
@@ -32,21 +33,8 @@ commit_changes() {
     fi
 
     echo "Committing Changes: $1"
-    
-    if ! git add . ; then
-        echo "Failed to add changes."
-        return 1
-    fi
-
-    if ! git commit -m "Auto commit on $(date)" ; then
-        echo "No changes to commit."
-        return 1
-    fi
-
-    if ! git push origin main ; then
-        echo "Failed to push changes."
-        return 1
-    fi
+    git commit -m "Auto commit on $(date)"
+    git push origin main
 }
 
 # Monitoring file changes with fswatch
